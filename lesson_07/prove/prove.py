648e7fdf1e531f251179f5865ef91abff12b411a
febarnett3 <113536068+febarnett3@users.noreply.github.com>
2025-06-10 19:10:30 -0600

    prove-in-progress

--- lesson_07/prove/prove.py
+++ lesson_07/prove/prove.py
@@ -1,17 +1,228 @@
-""" 
-Course: CSE 351
-Week  : 
-File  : 
-Author:  
+"""
+Course: CSE 251 
+Lesson: L07 Prove
+File:   prove.py
+Author: <Add name here>
+
+Purpose: Process Task Files.
+
+Instructions:
+
+See Canvas for the full instructions for this assignment. You will need to complete the TODO comment
+below before submitting this file:
+
+Note: each of the 5 task functions need to return a string.  They should not print anything.
+
+TODO:
+
+Add your comments here on the pool sizes that you used for your assignment and why they were the best choices.
 """
 
-# Include CSE 351 common Python files. 
+from datetime import datetime, timedelta
+import requests
+import multiprocessing as mp
+from matplotlib.pylab import plt
+import numpy as np
+import glob
+import math 
+
+# Include cse 251 common Python files - Dont change
 from cse351 import *
+
+# Constants - Don't change
+TYPE_PRIME  = 'prime'
+TYPE_WORD   = 'word'
+TYPE_UPPER  = 'upper'
+TYPE_SUM    = 'sum'
+TYPE_NAME   = 'name'
+
+# TODO: Change the pool sizes and explain your reasoning in the header comment
+
+PRIME_POOL_SIZE = 3
+SUM_POOL_SIZE   = 2
+UPPER_POOL_SIZE = 1
+WORD_POOL_SIZE  = 2
+NAME_POOL_SIZE  = 4
+
+# Global lists to collect the task results
+result_primes = []
+result_words = []
+result_upper = []
+result_sums = []
+result_names = []
+
+def is_prime(n: int):
+    """Primality test using 6k+-1 optimization.
+    From: https://en.wikipedia.org/wiki/Primality_test
+    """
+    if n <= 3:
+        return n > 1
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i ** 2 <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+def task_prime(value):
+    """
+    Use the is_prime() above
+    Add the following to the global list:
+        {value} is prime
+            - or -
+        {value} is not prime
+    """
+    if is_prime(value):
+        return f"{value} is prime"
+    else:
+        return f"{value} is not prime"
+
+
+def task_word(word):
+    """
+    search in file 'words.txt'
+    Add the following to the global list:
+        {word} Found
+            - or -
+        {word} not found *****
+    """
+    with open("words.txt", "r") as file:
+        for line in file:
+            if line.strip() == word:
+                return f"{word} found"
+        return f"{word} not found *****"
+
+
+def task_upper(text):
+    """
+    Add the following to the global list:
+        {text} ==>  uppercase version of {text}
+    """
+    return text.upper()
+
+
+def task_sum(start_value, end_value):
+    """
+    Add the following to the global list:
+        sum of all numbers between start_value and end_value
+        answer = {start_value:,} to {end_value:,} = {total:,}
+    """
+    total = sum(range(start_value, end_value + 1))
+    return (
+    f"sum of all numbers between {start_value:,} and {end_value:,}\n"
+    f"answer = {start_value:,} to {end_value:,} = {total:,}"
+    )
+
+
+def task_name(url):
+    """
+    use requests module
+    Add the following to the global list:
+        {url} has name <name>
+            - or -
+        {url} had an error receiving the information
+    """
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        name = data.get("name")
+        if name:
+            return f"{url} has name {name}"
+    return f"{url} had an error receiving the information"
 
 
 def main():
-    ...
+    log = Log(show_terminal=True)
+    log.start_timer()
+
+    # TODO Create process pools
+    prime_pool = mp.Pool(processes = PRIME_POOL_SIZE)
+    word_pool = mp.Pool(processes = WORD_POOL_SIZE)
+    upper_pool = mp.Pool(processes = UPPER_POOL_SIZE)
+    sum_pool = mp.Pool(processes = SUM_POOL_SIZE)
+    name_pool = mp.Pool(processes = NAME_POOL_SIZE)
+    
+
+    # TODO change the following if statements to start the pools
+    
+    count = 0
+    task_files = glob.glob("tasks/*.task")
+    for filename in task_files:
+        # print()
+        # print(filename)
+        task = load_json_file(filename)
+        print(task)
+        count += 1
+        task_type = task['task']
+        if task_type == TYPE_PRIME:
+            # task_prime(task['value'])
+            prime_pool.apply_async(task_prime, args=(task['value'],), callback=lambda result: result_primes.append(result))
+        elif task_type == TYPE_WORD:
+            # task_word(task['word'])
+            word_pool.apply_async(task_word, args=(task['word'],), callback=lambda result: result_words.append(result))
+        elif task_type == TYPE_UPPER:
+            # task_upper(task['text'])
+            upper_pool.apply_async(task_upper, args=(task['text'],), callback=lambda result: result_upper.append(result))
+        elif task_type == TYPE_SUM:
+            # task_sum(task['start'], task['end'])
+            sum_pool.apply_async(task_sum, args=(task['start'], task['end']), callback=lambda result: result_sums.append(result))
+        elif task_type == TYPE_NAME:
+            # task_name(task['url'])
+            name_pool.apply_async(task_name, args=(task['url'],), callback=lambda result: result_names.append(result))
+
+        else:
+            log.write(f'Error: unknown task type {task_type}')
+
+    # TODO wait on the pools
+    prime_pool.close()
+    word_pool.close()
+    upper_pool.close()
+    sum_pool.close()
+    name_pool.close()
+
+    prime_pool.join()
+    word_pool.join()
+    upper_pool.join()
+    sum_pool.join()
+    name_pool.join()
+
+    # DO NOT change any code below this line!
+    #---------------------------------------------------------------------------
+    def log_list(lst, log):
+        for item in lst:
+            log.write(item)
+        log.write(' ')
+    
+    log.write('-' * 80)
+    log.write(f'Primes: {len(result_primes)}')
+    log_list(result_primes, log)
+
+    log.write('-' * 80)
+    log.write(f'Words: {len(result_words)}')
+    log_list(result_words, log)
+
+    log.write('-' * 80)
+    log.write(f'Uppercase: {len(result_upper)}')
+    log_list(result_upper, log)
+
+    log.write('-' * 80)
+    log.write(f'Sums: {len(result_sums)}')
+    log_list(result_sums, log)
+
+    log.write('-' * 80)
+    log.write(f'Names: {len(result_names)}')
+    log_list(result_names, log)
+
+    log.write(f'Number of Primes tasks: {len(result_primes)}')
+    log.write(f'Number of Words tasks: {len(result_words)}')
+    log.write(f'Number of Uppercase tasks: {len(result_upper)}')
+    log.write(f'Number of Sums tasks: {len(result_sums)}')
+    log.write(f'Number of Names tasks: {len(result_names)}')
+    log.stop_timer(f'Total time to process {count} tasks')
 
 
-if __name__ == __main__:
+if __name__ == '__main__':
     main()
---
+++ lesson_08/prove/prove_part_1.py
@@ -0,0 +1,132 @@
+"""
+Course: CSE 251 
+Assignment: 08 Prove Part 1
+File:   prove_part_1.py
+Author: <Add name here>
+
+Purpose: Part 1 of assignment 8, finding the path to the end of a maze using recursion.
+
+Instructions:
+
+- Do not create classes for this assignment, just functions.
+- Do not use any other Python modules other than the ones included.
+- Complete any TODO comments.
+"""
+
+import math
+from screen import Screen
+from maze import Maze
+import cv2
+import sys
+
+# Include cse 351 files
+from cse351 import *
+
+SCREEN_SIZE = 800
+COLOR = (0, 0, 255)
+SLOW_SPEED = 100
+FAST_SPEED = 1
+speed = SLOW_SPEED
+
+# TODO: Add any functions needed here.
+
+def solve_path(maze):
+    """ Solve the maze and return the path found between the start and end positions.  
+        The path is a list of positions, (x, y) """
+    path = []
+
+    def dfs(position):
+        path.append(position) # add position
+        curr_row, curr_col = position # define row and col values from position
+        if maze.at_end(curr_row, curr_col): # check if maze is at end before checking moves
+            return True
+        open_moves = maze.get_possible_moves(curr_row, curr_col) # returns a list of possible moves
+        for move in open_moves:
+            if maze.can_move_here(move[0], move[1]): # making sure if move is valid
+                maze.move(move[0], move[1], COLOR) # change to red on display
+                if dfs(move): # RECURSION, for this move, add it to path, and iterate through its open_moves if not at end, will recurse until a true or false is returned in one of the rounds.
+                    return True
+                else:
+                    path.pop() # in the case that it's false return adica no open moves (can't move there!), remove move from path
+                    maze.restore(move[0], move[1]) # and change color to gray.
+        return False  # ensures recursion always returns a boolean
+
+    position = maze.get_start_pos()
+    dfs(position)
+    return path
+
+
+
+
+    # Hint: You can create an inner function to do the recursion
+
+
+def get_path(log, filename):
+    """ Do not change this function """
+    # 'Maze: Press "q" to quit, "1" slow drawing, "2" faster drawing, "p" to play again'
+    global speed
+
+    # create a Screen Object that will contain all of the drawing commands
+    screen = Screen(SCREEN_SIZE, SCREEN_SIZE)
+    screen.background((255, 255, 0))
+
+    maze = Maze(screen, SCREEN_SIZE, SCREEN_SIZE, filename)
+
+    path = solve_path(maze)
+
+    log.write(f'Drawing commands to solve = {screen.get_command_count()}')
+
+    done = False
+    while not done:
+        if screen.play_commands(speed): 
+            key = cv2.waitKey(0)
+            if key == ord('1'):
+                speed = SLOW_SPEED
+            elif key == ord('2'):
+                speed = FAST_SPEED
+            elif key == ord('q'):
+                exit()
+            elif key != ord('p'):
+                done = True
+        else:
+            done = True
+
+    return path
+
+
+def find_paths(log):
+    """ Do not change this function """
+
+    files = (
+        'very-small.bmp',
+        'very-small-loops.bmp',
+        'small.bmp',
+        'small-loops.bmp',
+        'small-odd.bmp',
+        'small-open.bmp',
+        'large.bmp',
+        'large-loops.bmp',
+        'large-squares.bmp',
+        'large-open.bmp'
+    )
+
+    log.write('*' * 40)
+    log.write('Part 1')
+    for filename in files:
+        filename = f'./mazes/{filename}'
+        log.write()
+        log.write(f'File: {filename}')
+        path = get_path(log, filename)
+        log.write(f'Found path has length     = {len(path)}')
+    log.write('*' * 40)
+
+
+def main():
+    """ Do not change this function """
+    sys.setrecursionlimit(5000)
+    log = Log(show_terminal=True)
+    find_paths(log)
+
+
+if __name__ == "__main__":
+    main()
